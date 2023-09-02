42a7b1c2e17068a5f16838b1e650bef68b2ceac0
lalit-rao <lalitmrao@gmail.com>
2023-09-03 03:29:11 +0530

    update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,32 @@ import './Layout.css';
 
 const Layout = () => {
     return (
-        <div>
+        <div className="main-layout">
+            <h1 className="heading-layout">RESRANK</h1>
+            <div className="main-bar-layout">
+                <div className="bar-layout-1">
+                    <p className="bar-layout-text">Upload</p>
+                    <p className="bar-layout-text">Job Description</p>
+                    <p className="bar-layout-text">Search</p>
+                </div>
+                <div className="bar-layout-2">
+                    <p className="bar-layout-text">Filter</p>
+                    <p className="bar-layout-text">Sort</p>
+                </div>
+            </div>
+            <div className="box-layout">
+                <div className="small-box-layout">
+                    <div className="small-box-layout-rectangle">
+                        <p className="small-box-layout-rectangle-text">Upload</p>
+                    </div>
+                    <div className="small-box-layout-rectangle-2">
+                        <p className="small-box-layout-rectangle-2-text">Drag and drop your files to Upload</p>
+                    </div>
+                    <div className="small-box-layout-rectangle-3">
+                        <p className="small-box-layout-rectangle-text-3">Select Files</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
